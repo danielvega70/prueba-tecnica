f828da8413e0ae82b3e19ab31d0b98d4d95bdf7d
danielvega70 <80554408+danielvega70@users.noreply.github.com>
2023-04-06 15:33:09 -0500

    Create pagina principal.php

---
+++ pagina principal.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mi cuestionario</title>
+</head>
+<body>
+	<h1>Mi cuestionario</h1>
+	<p>Bienvenido al cuestionario sobre tus gustos personales.</p>
+	<a href="crear_pregunta.php">Crear pregunta</a>
+	<a href="ver_preguntas.php">Ver preguntas</a>
+</body>
+</html>
